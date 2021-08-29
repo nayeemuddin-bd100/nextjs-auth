31fb82f28a3d5fe31fdc7239ea47633b8aa548a3
Nayeem <nayeemuddin.bd100@gmail.com>
2021-08-29 20:59:10 +0600

    added sendgrid

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,25 +7,30 @@ const options = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-   /*  Providers.Facebook({
+    /*  Providers.Facebook({
       clientId: process.env.FACEBOOK_ID,
       clientSecret: process.env.FACEBOOK_SECRET,
     }), */
-    /* Providers.Email({
+    Providers.Email({
       server: {
-        host: "",
-        port: "",
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
         auth: {
-          user: "",
-          pass: "",
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
         },
       },
-      form: "",
-    }), */
+      form: process.env.EMAIL_FORM,
+    }),
   ],
+  database: {
+    type: "sqlite",
+    database: ":memory:",
+    synchronize: true,
+  },
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
-// export default (req, res) => NextAuth(req, res, options);
-export default NextAuth(options)
+export default (req, res) => NextAuth(req, res, options);
+// export default NextAuth(options)
 
